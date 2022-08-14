66d3b6af7d7eb7eb68fcaf1bcadd08f40d6d16a4
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2022-08-14 15:07:50 +0200

    added users route

--- movieHandlers.js
+++ movieHandlers.js
@@ -1,3 +1,5 @@
+const database = require('./database');
+
 const movies = [
   {
     id: 1,
@@ -26,19 +28,34 @@ const movies = [
 ];
 
 const getMovies = (req, res) => {
-  res.json(movies);
+  database
+    .query('select * from movies')
+    .then(([movies]) => {
+      res.status(200).json(movies);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error retrieving data from database');
+    });
 };
 
 const getMovieById = (req, res) => {
   const id = parseInt(req.params.id);
 
-  const movie = movies.find((movie) => movie.id === id);
+  database
+    .query('select * from movies where id=?', [id])
+    .then(([movie]) => {
+      if (movie.length === 0) {
+        res.status(404).send('Not found');
+      } else {
+        res.status(200).json(movie);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error retrieving data from database');
+    });
 
-  if (movie != null) {
-    res.json(movie);
-  } else {
-    res.status(404).send("Not Found");
-  }
 };
 
 module.exports = {
