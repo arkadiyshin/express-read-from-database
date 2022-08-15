170430b7d84538701f18c1400854e20d50da639c
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2022-08-15 11:05:26 +0200

    express-put-method

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 require('dotenv').config();
 
 const port = process.env.APP_PORT ?? 5000;
 
 const welcome = (req, res) => {
-  res.send("Welcome to my favourite movie list");
+    res.send("Welcome to my favourite movie list");
 };
 
 app.get("/", welcome);
@@ -16,16 +18,20 @@ const movieHandlers = require("./movieHandlers");
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.post("/api/movies", movieHandlers.postMovie);
 
 const userHandlers = require("./userHandlers");
 
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
+app.post("/api/users", userHandlers.postUser);
+app.put("/api/users", userHandlers.updateUser);
+
 
 app.listen(port, (err) => {
-  if (err) {
-    console.error("Something bad happened");
-  } else {
-    console.log(`Server is listening on ${port}`);
-  }
+    if (err) {
+        console.error("Something bad happened");
+    } else {
+        console.log(`Server is listening on ${port}`);
+    }
 });
--- userHandlers.js
+++ userHandlers.js
@@ -31,7 +31,50 @@ const getUserById = (req, res) => {
 
 };
 
+const postUser = (req, res) => {
+    const { firstname, lastname, email, city, language } = req.body;
+
+    database
+        .query(
+            "INSERT INTO users(firstname, lastname, email, city, language) VALUES (?, ?, ?, ?, ?)",
+            [firstname, lastname, email, city, language]
+        )
+        .then(([result]) => {
+            res.location(`/api/users/${result.insertId}`).sendStatus(201);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send("Error saving the user");
+        });
+
+};
+
+const updateUser = (req, res) => {
+    
+    const id = parseInt(req.params.id);
+    const { firstname, lastname, email, city, language } = req.body;
+
+    database
+        .query(
+            "update users set firstname = ?, lastname = ?, email = ?, city = ?, language = ? where id = ?",
+            [firstname, lastname, email, city, language, id]
+        )
+        .then(([result]) => {
+            if (result.affectedRows === 0) {
+                res.status(404).send("Not Found");
+            } else {
+                res.sendStatus(204);
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send("Error editing the user");
+        });
+};
+
 module.exports = {
     getUsers,
     getUserById,
+    postUser,
+    updateUser,
 };
