b238ee43858960879f5857d3505e09996a6e3b52
arkadiyshin <108717969+arkadiyshin@users.noreply.github.com>
2022-08-15 11:20:23 +0200

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const userHandlers = require("./userHandlers");
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
 app.post("/api/users", userHandlers.postUser);
-app.put("/api/users", userHandlers.updateUser);
+app.put("/api/users/:id", userHandlers.updateUser);
 
 
 app.listen(port, (err) => {
